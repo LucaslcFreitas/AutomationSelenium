4b08030857900081dda1dbfd155c64d27ab233a4
Lucas Freitas <lucaslcfjf@hotmail.com>
2021-08-05 09:58:37 -0300

    Configura as consultas do Selenium WebDriver

--- src/main/java/com/automationselenium/Main.java
+++ src/main/java/com/automationselenium/Main.java
@@ -1,13 +1,76 @@
 package com.automationselenium;
 
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
 /**
  *
  * @author Lucas
  */
 public class Main {
+    public static String months[] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+    public static String years[] = {"2017", "2018", "2019", "2020", "2021"};
+    public static int start_period[] = {0, 2};
+    public static int end_period[] = {11, 3};
+    public static String local = "JUIZ DE FORA";
     
     public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "local");
+        WebDriver browser = new FirefoxDriver();
+        int count = 0, current_month = start_period[0], current_year = start_period[1];
         
+        
+        browser.get("https://www.detran.mg.gov.br/parceiros-credenciados/centro-de-formacao-de-condutores/consultar-produtividade-dos-cfcs");
+        
+        while(current_month != end_period[0] || current_year != end_period[1]) {
+            //Seleciona a cidade
+            Select city = new Select(browser.findElement(By.name("cidade")));
+            city.selectByVisibleText(local);
+            
+            //Seleciona o período inicial
+            Select startPeriod = new Select(browser.findElement(By.name("periodo_inicio")));
+            startPeriod.selectByVisibleText(months[current_month]+"/"+years[current_year]);
+            
+            //Seleciona o período final
+            Select endPeriod = new Select(browser.findElement(By.name("periodo_fim")));
+            endPeriod.selectByVisibleText(months[current_month]+"/"+years[current_year]);
+            
+            //Tipo de consulta
+            browser.findElement(By.id("tipo-consulta-d")).click();
+            
+            //Seleciona o tipo de exame
+            Select typeExam = new Select(browser.findElement(By.name("tipo_exame")));
+            typeExam.selectByValue("2");
+            
+            browser.findElement(By.id("pesquisar")).click();
+            
+            List<WebElement> row = browser.findElements(By.xpath("//table/tbody/tr/td[1]"));
+            
+            System.out.println(months[current_month]+"/"+years[current_year]);
+            System.out.println("Linhas: " + row.size());
+            
+            if(row.size() > 0) {
+                for(int i=1; i<=row.size(); i++) {
+                    String name = browser.findElement(By.xpath("//table/tbody/tr["+i+"]/td[3]")).getText();
+                    float percent = Float.parseFloat(browser.findElement(By.xpath("//table/tbody/tr["+i+"]/td[4]")).getText().replace("%", "").trim());
+                    System.out.println(name + ": " + percent);
+                }
+            }
+            
+            browser.findElement(By.xpath("//html/body/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[2]/div/a")).click();
+            
+            count = count + 1;
+            current_month = current_month + 1;
+            if(current_month == 12) {
+                current_month = 0;
+                current_year = current_year + 1;
+            }
+        }
+        
+        browser.quit();
     }
-    
 }
